680cd2679dc0e0d60990b7828c64b6b7871a2d65
Timothy Chen <tnachen@gmail.com>
2017-06-09 14:07:43 -0700

    Seperate out load test method

--- main.go
+++ main.go
@@ -239,6 +239,200 @@ func shouldCheckHash(sampleRate float64) bool {
 	return rand.Float64() < sampleRate
 }
 
+type RunLoadParams struct {
+	qps                int
+	concurrency        int
+	method             string
+	interval           time.Duration
+	noreuse            bool
+	compress           bool
+	noLatencySummary   bool
+	reportLatenciesCSV string
+	totalRequests      uint64
+	headers            headerSet
+	metricAddr         string
+	hashValue          uint64
+	hashSampleRate     float64
+	dstURL             url.URL
+	hosts              []string
+	requestData        []byte
+}
+
+func RunLoad(params RunLoadParams) {
+	// Repsonse tracking metadata.
+	count := uint64(0)
+	size := uint64(0)
+	good := uint64(0)
+	bad := uint64(0)
+	failed := uint64(0)
+	min := int64(math.MaxInt64)
+	max := int64(0)
+	failedHashCheck := int64(0)
+
+	hist := hdrhistogram.New(0, DayInMs, 3)
+	globalHist := hdrhistogram.New(0, DayInMs, 3)
+	latencyHistory := ring.New(5)
+	received := make(chan *MeasuredResponse)
+	timeout := time.After(params.interval)
+	timeToWait := CalcTimeToWait(&params.qps)
+	var totalTrafficTarget int
+	totalTrafficTarget = params.qps * params.concurrency * int(params.interval.Seconds())
+
+	doTLS := params.dstURL.Scheme == "https"
+	client := newClient(params.compress, doTLS, params.noreuse, params.concurrency)
+	var sendTraffic sync.WaitGroup
+	// The time portion of the header can change due to timezone.
+	timeLen := len(time.Now().Format(time.RFC3339))
+	timePadding := strings.Repeat(" ", timeLen)
+	intLen := len(fmt.Sprintf("%s", params.interval))
+	intPadding := strings.Repeat(" ", intLen-2)
+
+	fmt.Printf("# sending %d %s req/s with concurrency=%d to %s ...\n", (params.qps * params.concurrency), params.method, params.concurrency, params.dstURL)
+	fmt.Printf("# %s good/b/f t   goal%% %s min [p50 p95 p99  p999]  max bhash change\n", timePadding, intPadding)
+	for i := 0; i < params.concurrency; i++ {
+		ticker := time.NewTicker(timeToWait)
+		go func() {
+			// For each goroutine we want to reuse a buffer for performance reasons.
+			bodyBuffer := make([]byte, 50000)
+			sendTraffic.Add(1)
+			for _ = range ticker.C {
+				var checkHash bool
+				hasher := fnv.New64a()
+				if params.hashSampleRate > 0.0 {
+					checkHash = shouldCheckHash(params.hashSampleRate)
+				} else {
+					checkHash = false
+				}
+				shouldFinishLock.RLock()
+				if !shouldFinish {
+					shouldFinishLock.RUnlock()
+					sendRequest(client, params.method, &params.dstURL, params.hosts[rand.Intn(len(params.hosts))], params.headers, params.requestData, atomic.AddUint64(&reqID, 1), params.hashValue, checkHash, hasher, received, bodyBuffer)
+				} else {
+					shouldFinishLock.RUnlock()
+					sendTraffic.Done()
+					return
+				}
+			}
+		}()
+	}
+
+	cleanup := make(chan bool, 2)
+	interrupted := make(chan os.Signal, 2)
+	signal.Notify(interrupted, syscall.SIGINT)
+
+	if params.metricAddr != "" {
+		registerMetrics()
+		go func() {
+			http.Handle("/metrics", promhttp.Handler())
+			http.ListenAndServe(params.metricAddr, nil)
+		}()
+	}
+
+	for {
+		select {
+		// If we get a SIGINT, then start the shutdown process.
+		case <-interrupted:
+			cleanup <- true
+		case <-cleanup:
+			finishSendingTraffic()
+			if !params.noLatencySummary {
+				hdrreport.PrintLatencySummary(globalHist)
+			}
+			if params.reportLatenciesCSV != "" {
+				err := hdrreport.WriteReportCSV(&params.reportLatenciesCSV, globalHist)
+				if err != nil {
+					log.Panicf("Unable to write Latency CSV file: %v\n", err)
+				}
+			}
+			go func() {
+				// Don't Wait() in the event loop or else we'll block the workers
+				// from draining.
+				sendTraffic.Wait()
+				os.Exit(0)
+			}()
+		case t := <-timeout:
+			// When all requests are failures, ensure we don't accidentally
+			// print out a monstrously huge number.
+			if min == math.MaxInt64 {
+				min = 0
+			}
+			// Periodically print stats about the request load.
+			percentAchieved := int(math.Min((((float64(good) + float64(bad)) /
+				float64(totalTrafficTarget)) * 100), 100))
+
+			lastP99 := int(hist.ValueAtQuantile(99))
+			// We want the change indicator to be based on
+			// how far away the current value is from what
+			// we've seen historically. This is why we call
+			// CalculateChangeIndicator() first and then Push()
+			changeIndicator := window.CalculateChangeIndicator(latencyHistory.Items, lastP99)
+			latencyHistory.Push(lastP99)
+
+			fmt.Printf("%s %6d/%1d/%1d %d %3d%% %s %3d [%3d %3d %3d %4d ] %4d %6d %s\n",
+				t.Format(time.RFC3339),
+				good,
+				bad,
+				failed,
+				totalTrafficTarget,
+				percentAchieved,
+				params.interval,
+				min,
+				hist.ValueAtQuantile(50),
+				hist.ValueAtQuantile(95),
+				hist.ValueAtQuantile(99),
+				hist.ValueAtQuantile(999),
+				max,
+				failedHashCheck,
+				changeIndicator)
+
+			count = 0
+			size = 0
+			good = 0
+			bad = 0
+			min = math.MaxInt64
+			max = 0
+			failed = 0
+			failedHashCheck = 0
+			hist.Reset()
+			timeout = time.After(params.interval)
+
+			if params.totalRequests != 0 && reqID > params.totalRequests {
+				cleanup <- true
+			}
+		case managedResp := <-received:
+			count++
+			promRequests.Inc()
+			if managedResp.err != nil {
+				fmt.Fprintln(os.Stderr, managedResp.err)
+				failed++
+			} else {
+				size += managedResp.sz
+				if managedResp.failedHashCheck {
+					failedHashCheck++
+				}
+				if managedResp.code >= 200 && managedResp.code < 500 {
+					good++
+					promSuccesses.Inc()
+					promLatencyHistogram.Observe(float64(managedResp.latency))
+				} else {
+					bad++
+				}
+
+				if managedResp.latency < min {
+					min = managedResp.latency
+				}
+
+				if managedResp.latency > max {
+					max = managedResp.latency
+				}
+
+				hist.RecordValue(managedResp.latency)
+				globalHist.RecordValue(managedResp.latency)
+			}
+		}
+	}
+}
+
 func main() {
 	qps := flag.Int("qps", 1, "QPS to send to backends per request thread")
 	concurrency := flag.Int("concurrency", 1, "Number of request threads")
@@ -293,176 +487,24 @@ func main() {
 
 	requestData := loadData(*data)
 
-	// Repsonse tracking metadata.
-	count := uint64(0)
-	size := uint64(0)
-	good := uint64(0)
-	bad := uint64(0)
-	failed := uint64(0)
-	min := int64(math.MaxInt64)
-	max := int64(0)
-	failedHashCheck := int64(0)
-
-	hist := hdrhistogram.New(0, DayInMs, 3)
-	globalHist := hdrhistogram.New(0, DayInMs, 3)
-	latencyHistory := ring.New(5)
-	received := make(chan *MeasuredResponse)
-	timeout := time.After(*interval)
-	timeToWait := CalcTimeToWait(qps)
-	var totalTrafficTarget int
-	totalTrafficTarget = *qps * *concurrency * int(interval.Seconds())
-
-	doTLS := dstURL.Scheme == "https"
-	client := newClient(*compress, doTLS, *noreuse, *concurrency)
-	var sendTraffic sync.WaitGroup
-	// The time portion of the header can change due to timezone.
-	timeLen := len(time.Now().Format(time.RFC3339))
-	timePadding := strings.Repeat(" ", timeLen)
-	intLen := len(fmt.Sprintf("%s", *interval))
-	intPadding := strings.Repeat(" ", intLen-2)
-
-	fmt.Printf("# sending %d %s req/s with concurrency=%d to %s ...\n", (*qps * *concurrency), *method, *concurrency, dstURL)
-	fmt.Printf("# %s good/b/f t   goal%% %s min [p50 p95 p99  p999]  max bhash change\n", timePadding, intPadding)
-	for i := 0; i < *concurrency; i++ {
-		ticker := time.NewTicker(timeToWait)
-		go func() {
-			// For each goroutine we want to reuse a buffer for performance reasons.
-			bodyBuffer := make([]byte, 50000)
-			sendTraffic.Add(1)
-			for _ = range ticker.C {
-				var checkHash bool
-				hasher := fnv.New64a()
-				if *hashSampleRate > 0.0 {
-					checkHash = shouldCheckHash(*hashSampleRate)
-				} else {
-					checkHash = false
-				}
-				shouldFinishLock.RLock()
-				if !shouldFinish {
-					shouldFinishLock.RUnlock()
-					sendRequest(client, *method, dstURL, hosts[rand.Intn(len(hosts))], headers, requestData, atomic.AddUint64(&reqID, 1), *hashValue, checkHash, hasher, received, bodyBuffer)
-				} else {
-					shouldFinishLock.RUnlock()
-					sendTraffic.Done()
-					return
-				}
-			}
-		}()
+	params := RunLoadParams{
+		qps:                *qps,
+		concurrency:        *concurrency,
+		method:             *method,
+		interval:           *interval,
+		noreuse:            *noreuse,
+		compress:           *compress,
+		noLatencySummary:   *noLatencySummary,
+		reportLatenciesCSV: *reportLatenciesCSV,
+		totalRequests:      *totalRequests,
+		headers:            headers,
+		metricAddr:         *metricAddr,
+		hashValue:          *hashValue,
+		hashSampleRate:     *hashSampleRate,
+		dstURL:             *dstURL,
+		hosts:              hosts,
+		requestData:        requestData,
 	}
 
-	cleanup := make(chan bool, 2)
-	interrupted := make(chan os.Signal, 2)
-	signal.Notify(interrupted, syscall.SIGINT)
-
-	if *metricAddr != "" {
-		registerMetrics()
-		go func() {
-			http.Handle("/metrics", promhttp.Handler())
-			http.ListenAndServe(*metricAddr, nil)
-		}()
-	}
-
-	for {
-		select {
-		// If we get a SIGINT, then start the shutdown process.
-		case <-interrupted:
-			cleanup <- true
-		case <-cleanup:
-			finishSendingTraffic()
-			if !*noLatencySummary {
-				hdrreport.PrintLatencySummary(globalHist)
-			}
-			if *reportLatenciesCSV != "" {
-				err := hdrreport.WriteReportCSV(reportLatenciesCSV, globalHist)
-				if err != nil {
-					log.Panicf("Unable to write Latency CSV file: %v\n", err)
-				}
-			}
-			go func() {
-				// Don't Wait() in the event loop or else we'll block the workers
-				// from draining.
-				sendTraffic.Wait()
-				os.Exit(0)
-			}()
-		case t := <-timeout:
-			// When all requests are failures, ensure we don't accidentally
-			// print out a monstrously huge number.
-			if min == math.MaxInt64 {
-				min = 0
-			}
-			// Periodically print stats about the request load.
-			percentAchieved := int(math.Min((((float64(good) + float64(bad)) /
-				float64(totalTrafficTarget)) * 100), 100))
-
-			lastP99 := int(hist.ValueAtQuantile(99))
-			// We want the change indicator to be based on
-			// how far away the current value is from what
-			// we've seen historically. This is why we call
-			// CalculateChangeIndicator() first and then Push()
-			changeIndicator := window.CalculateChangeIndicator(latencyHistory.Items, lastP99)
-			latencyHistory.Push(lastP99)
-
-			fmt.Printf("%s %6d/%1d/%1d %d %3d%% %s %3d [%3d %3d %3d %4d ] %4d %6d %s\n",
-				t.Format(time.RFC3339),
-				good,
-				bad,
-				failed,
-				totalTrafficTarget,
-				percentAchieved,
-				interval,
-				min,
-				hist.ValueAtQuantile(50),
-				hist.ValueAtQuantile(95),
-				hist.ValueAtQuantile(99),
-				hist.ValueAtQuantile(999),
-				max,
-				failedHashCheck,
-				changeIndicator)
-
-			count = 0
-			size = 0
-			good = 0
-			bad = 0
-			min = math.MaxInt64
-			max = 0
-			failed = 0
-			failedHashCheck = 0
-			hist.Reset()
-			timeout = time.After(*interval)
-
-			if *totalRequests != 0 && reqID > *totalRequests {
-				cleanup <- true
-			}
-		case managedResp := <-received:
-			count++
-			promRequests.Inc()
-			if managedResp.err != nil {
-				fmt.Fprintln(os.Stderr, managedResp.err)
-				failed++
-			} else {
-				size += managedResp.sz
-				if managedResp.failedHashCheck {
-					failedHashCheck++
-				}
-				if managedResp.code >= 200 && managedResp.code < 500 {
-					good++
-					promSuccesses.Inc()
-					promLatencyHistogram.Observe(float64(managedResp.latency))
-				} else {
-					bad++
-				}
-
-				if managedResp.latency < min {
-					min = managedResp.latency
-				}
-
-				if managedResp.latency > max {
-					max = managedResp.latency
-				}
-
-				hist.RecordValue(managedResp.latency)
-				globalHist.RecordValue(managedResp.latency)
-			}
-		}
-	}
+	RunLoad(params)
 }
