8ab9ac71b29ec908b34ccc09afca1b9e9fc370e5
William Chen <chen.biwei@gmail.com>
2017-10-16 13:56:40 +0800

    remove comment lines, uncomment test case

--- load/run_load_test.go
+++ load/run_load_test.go
@@ -25,18 +25,18 @@ func TestRunLoad(t *testing.T) {
 		svr.ListenAndServe()
 	}()
 
-	// t.Run("Test data from stdin", func(t *testing.T) {
-	// 	t.Parallel()
-	// 	testData = "blahblah=123"
-	// 	err = run(testData)
-	// 	assert.Empty(t, err, "test load data from stdin: %v", err)
-	// })
-	// t.Run("Test data from file", func(t *testing.T) {
-	// 	t.Parallel()
-	// 	testData = "@test.txt"
-	// 	err = run(testData)
-	// 	assert.Empty(t, err, "test load file error: %v", err)
-	// })
+	t.Run("Test data from stdin", func(t *testing.T) {
+		t.Parallel()
+		testData = "blahblah=123"
+		err = run(testData)
+		assert.Empty(t, err, "test load data from stdin: %v", err)
+	})
+	t.Run("Test data from file", func(t *testing.T) {
+		t.Parallel()
+		testData = "@test.txt"
+		err = run(testData)
+		assert.Empty(t, err, "test load file error: %v", err)
+	})
 	// data from remote url
 	t.Run("Test data from remote file", func(t *testing.T) {
 		t.Parallel()
